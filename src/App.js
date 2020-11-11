ee96b655fcecc79c7562c440254ef8569140a075
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-11-11 16:11:44 +0000

    Handle the Error page

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import { Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms" component={Rooms} />
-      <Route exact path="/rooms/:slug" component={SingleRoom} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Error() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hello from the Error page</h1>
+    </div>
+  );
 }
 
 export default Error;
