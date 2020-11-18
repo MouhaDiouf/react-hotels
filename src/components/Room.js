c59a986f86e1403ced58c8f584ca62b9f94fdb91
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-11-18 15:11:38 +0000

    Add a defaultimage for featured images as a fallback

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import defaultImg from '../images/room-1.jpeg';
 function Room({ room }) {
   const { name, slug, images, price } = room;
   return (
     <article className="room">
       <div className="img-container">
-        <img src={images[0]} alt="single room" />
+        <img src={images[0] || defaultImg} alt="single room" />
       </div>
     </article>
   );
