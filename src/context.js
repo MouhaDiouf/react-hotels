da46ca7032b6ba2562f275147c148bddfd7881e2
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-11-19 20:35:51 +0000

    Make request to the api made with cotentful

--- src/context.js
+++ src/context.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Loading from './components/Loading';
 import items from './data';
+import Client from './contenful';
+
 const RoomContext = React.createContext();
 // <RoomContext.Provider value={'hello'}
 
@@ -20,21 +22,31 @@ class RoomProvider extends Component {
     breakfast: false,
     pets: false,
   };
+  getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: 'beachResortRoom',
+      });
+      console.log(response);
+      let rooms = this.formatData(response.items);
+      let featuredRooms = rooms.filter((room) => room.featured === true);
+      let maxPrice = Math.max(...rooms.map((item) => item.price));
+      let maxSize = Math.max(...rooms.map((item) => item.size));
 
+      this.setState({
+        rooms,
+        featuredRooms,
+        sortedRooms: rooms,
+        loading: false,
+        maxPrice,
+        maxSize,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   componentDidMount() {
-    let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter((room) => room.featured === true);
-    let maxPrice = Math.max(...rooms.map((item) => item.price));
-    let maxSize = Math.max(...rooms.map((item) => item.size));
-
-    this.setState({
-      rooms,
-      featuredRooms,
-      sortedRooms: rooms,
-      loading: false,
-      maxPrice,
-      maxSize,
-    });
+    this.getData();
   }
 
   getRoom = (slug) => {
